e0e9423d13540733dd9b55db0a42aaa4d480273e
JangHyoSeong <wkdgytmd200@naver.com>
2023-05-30 01:37:18 +0900

    nav

--- DBProject/urls.py
+++ DBProject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,10 @@ from . import views
 from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 app_name = "user"
 
 urlpatterns = [
-    
+    path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
 ]
